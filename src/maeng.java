a03910685a9e236df779cc8d44101da6fcd6caf2
Tobias Laundal <tobias.laundal@gamil.com>
2011-12-08 20:54:29 +0100

    Checking that everiting for 0014 is out!

---
+++ src/maeng.java
@@ -0,0 +1,10 @@
+import java.awt.Image;
+
+
+public class maeng extends Character{
+	
+	public maeng(String name, Image[] ar, Image[] al, Image[] au, Image[] ad, double x, double y, float vxr, float vyr, float vxl, float vyl, float vxu, float vyu, float vxd, float vyd, String currentimage, int lives){
+		super(name, ar, al, au, ad, ad, x, y, vxr,vyr, vxl, vyl, vxu,vyu,vxd,vyd, currentimage, lives);
+	}
+	
+}
