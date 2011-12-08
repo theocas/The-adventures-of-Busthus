1fd290b0933ce9835215c20d0e44f7d91dd350c4
Tobias Laundal <tobias.laundal@gamil.com>
2011-12-08 20:53:19 +0100

    Checking that everiting for 0014 is out!

---
+++ src/Busthus.java
@@ -0,0 +1,12 @@
+import java.awt.Image;
+
+
+public class Busthus extends Character{
+
+	
+	
+	public Busthus(String name, Image[] ar, Image[] al, Image[] au, Image[] ad, Image[] attacks, double x, double y, float vxr, float vyr, float vxl, float vyl, float vxu, float vyu, float vxd, float vyd, String currentimage, int lives){
+		super(name, ar, al, au, ad, attacks, x, y, vxr,vyr, vxl, vyl, vxu,vyu,vxd,vyd, currentimage, lives);
+	}
+	
+}
